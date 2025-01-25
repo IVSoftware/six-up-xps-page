0ee6ca518059b7ce07871ee7670711096f8c01e4
Tom Gregor <tgregor@ivsoftware.com>
2025-01-25 13:56:54 -0700

    inprog

--- six-up-xps-page/MainWindow.xaml.cs
+++ six-up-xps-page/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZXing;
 
 namespace six_up_xps_page
 {
@@ -38,14 +41,40 @@ namespace six_up_xps_page
     }
     partial class UniqueNumberItem : ObservableObject
     {
-        [ObservableProperty]
-        string? wnr = GenerateName("WNR", 10);
+        public ImageSource QRCode
+        {
+            get
+            {
+                if (qrCode is null)
+                {
+                    qrCode = GenerateQRCode(UniqueNumber);
+                }
+                return qrCode;
+            }
+        }
+        ImageSource? qrCode = default;
+
 
         [ObservableProperty]
-        string? uniqueNumber = GenerateName("UN", 7);
+        string wNR = GenerateName("WNR", 10);
 
         [ObservableProperty]
-        string? matnr = GenerateName("MAT-", 7);
+        string uniqueNumber = GenerateName("UN", 7);
+
+        [ObservableProperty]
+        string matnr = GenerateName("MAT-", 7);
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            switch (e.PropertyName)
+            {
+                case nameof(UniqueNumber):
+                    qrCode = GenerateQRCode(UniqueNumber);
+                    OnPropertyChanged(nameof(QRCode));
+                    break;
+            }
+        }
 
         private static string GenerateName(string prefix, int length) =>
             $"{prefix}{
@@ -55,5 +84,21 @@ namespace six_up_xps_page
                 .ToUpper()
                 .Replace("-", string.Empty
                     ).Substring(0,length)}";
+        
+        private static ImageSource GenerateQRCode(string qrText)
+        {
+            var writer = new BarcodeWriter<WriteableBitmap>
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Height = 150,
+                    Width = 150,
+                    Margin = 0
+                },
+            };
+            WriteableBitmap qrCodeBitmap = writer.Write(qrText);
+            return qrCodeBitmap;
+        }
     }
 }
