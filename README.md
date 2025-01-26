c47b0e2849ed8b03b10aebff94b55fbf3fc8ce30
Tom Gregor <tgregor@ivsoftware.com>
2025-01-25 17:20:02 -0700

    nbrp

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# six-up-xps-page
+This is an interesting question with a lot of nuances. You'll have to forgive me if my example or approach isn't an "exact" fit for what you're doing. However, you asked it this way:
+
+>Is there any better way to get this done?
+
+If I'm following your code and intent, then yes the better way would be making sure that the _document_ (the data) is being properly decoupled from the view that displays it. because I don't believe you should be have to clone anything, but especially not framework elements. From reading your code it implies the existence of some knd of record that might look similar to this portable representation:
---
+++ six-up-xps-page/StringToQRCodeConverter.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using ZXing;
+
+namespace six_up_xps_page
+{
+    class StringToQRCodeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string qrText)
+            {
+                var bitMatrix = new ZXing.QrCode.QRCodeWriter().encode(qrText, BarcodeFormat.QR_CODE, 150, 150);
+                int width = bitMatrix.Width;
+                int height = bitMatrix.Height;
+                int stride = (width + 7) / 8;
+                byte[] pixels = new byte[width * height];
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        pixels[y * width + x] = bitMatrix[x, y] ? (byte)0 : (byte)255;
+                    }
+                }
+                BitmapSource bitmapSource = BitmapSource.Create(
+                    width,
+                    height,
+                    96,
+                    96,
+                    PixelFormats.Gray8,
+                    null,
+                    pixels,
+                    width
+                );
+                return bitmapSource;
+            }
+            else return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
